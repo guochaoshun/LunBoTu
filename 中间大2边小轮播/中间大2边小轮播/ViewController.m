f40f978b70fb588d01e24ac144a557abcbd2aa39
guocs <guocs@zwy.com>
2019-09-09 15:39:09 +0800

    最后的提交

--- 中间大2边小轮播/中间大2边小轮播/ViewController.m
+++ 中间大2边小轮播/中间大2边小轮播/ViewController.m
@@ -24,13 +24,13 @@
     // 主要参考代码 : https://github.com/orzzh/WLScrollView , 这个是用scrollview做的,然后自己写的cell复用,干嘛这么复杂啊,所以我的例子直接用了UICollectionView
     MainADView * main = [[MainADView alloc] initWithFrame:CGRectMake(0, 100, self.view.frame.size.width, 200)];
     main.dataSourse = @[@"1",@"2",@"3",@"4",];
-    main.timerInterval = 3;
+    main.timerInterval = 5;
     [main setCallBack:^(id  _Nonnull data) {
-        NSLog(@"对应的model数据 : %@",data);
+        NSLog(@"对应的model数据 :%@ %@",self,data);
     }];
     [self.view addSubview:main];
 
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(40 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(600 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [main removeFromSuperview];
     });
     
